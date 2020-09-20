943466c711d78377e3b99beb8c46238b5b89bce7
wakka-2 <wakka-2@hotmail.com>
2020-09-20 22:33:30 +0300

    fixing crash when profile pic is deleted

--- app/src/main/java/com/task/app/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/task/app/ui/fragments/ProfileFragment.kt
@@ -1,14 +1,18 @@
 package com.task.app.ui.fragments
 
+import android.content.Context
+import android.database.Cursor
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import com.task.app.R
 import com.task.app.model.User
@@ -16,7 +20,6 @@ import com.task.app.ui.fragments.ProfileFragmentDirections.Companion.actionProfi
 import com.task.app.util.base.BaseFragment
 import com.task.app.util.shared.SharedPrefsUtil
 import kotlinx.android.synthetic.main.fragment_profile.*
-import java.io.File
 
 
 class ProfileFragment : BaseFragment() {
@@ -45,23 +48,49 @@ class ProfileFragment : BaseFragment() {
         tvPhone.text = user?.phoneNum
         tvEmail.text = user?.email
         if (user?.image?.isNotEmpty()!!) {
-            var bitmap:Bitmap ?= null
-            if (Build.VERSION.SDK_INT < 28){
+            var bitmap: Bitmap? = null
+            if (Build.VERSION.SDK_INT < 28) {
                 bitmap = MediaStore.Images.Media.getBitmap(
-                requireActivity().contentResolver,
-                Uri.parse(user.image))
-            }else{
-                val source = ImageDecoder.createSource(
-                    requireContext().contentResolver,
+                    requireActivity().contentResolver,
                     Uri.parse(user.image)
                 )
-                val uri = Uri.parse(user.image)
-                if(File(uri.path!!).exists()) {
+            } else {
+                try {
+                    val source = ImageDecoder.createSource(
+                        requireContext().contentResolver,
+                        Uri.parse(user.image)
+                    )
                     bitmap = ImageDecoder.decodeBitmap(source)
+                } catch (e: Exception) {
+                    //Image is deleted
+                    Toast.makeText(requireContext(), "Image is deleted/n$e", Toast.LENGTH_LONG)
+                        .show()
                 }
             }
-            ivProfile.setImageBitmap(bitmap)
+            if (bitmap != null) {
+                ivProfile.setImageBitmap(bitmap)
+            }
         }
+    }
+
+    private fun getRealPathFromURI_API19(context: Context, uri: Uri?): String {
+        var filePath = ""
+        val wholeID = DocumentsContract.getDocumentId(uri)
+        // Split at colon, use second item in the array
+        val id = wholeID.split(":").toTypedArray()[1]
+        val column = arrayOf(MediaStore.Audio.Media.DATA)
+        // where id is equal to
+        val sel = MediaStore.Images.Media._ID + "=?"
+        val cursor: Cursor? = context.contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            column, sel, arrayOf(id), null
+        )
+        val columnIndex: Int = cursor?.getColumnIndex(column[0])!!
+        if (cursor.moveToFirst()) {
+            filePath = cursor.getString(columnIndex)
+        }
+        cursor.close()
+        return filePath
     }
 
     private fun navigationClicks(view: View) {
